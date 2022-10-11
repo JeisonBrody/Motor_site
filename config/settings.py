015391420756abe90aad9fc8b52398acce4f3874
alexander.gr2014@yandex.ru <gfgf123kjhfqyF>
2022-10-11 12:56:27 +0300

    Add base dir

--- config/settings.py
+++ config/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / "main/templates",
+                 ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
